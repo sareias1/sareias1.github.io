1a6802c2b88398a60eb7b7b55c56af57ae1cefd1
sabareias <sabrinaareias14@gmail.com>
2025-06-04 14:40:10 -0400

    Update projects, scripts, and add pomopet showcase

--- scripts.js
+++ scripts.js
@@ -1,12 +1,18 @@
 const modal = document.getElementById("modal");
 const modalContent = document.getElementById("modalContent");
 const invManModal = document.getElementById("invManModal");
+const pomoModal = document.getElementById("pomoModal");
 const closeSpan = document.getElementsByClassName("close")[0];
 
 // modal functions:
 invManModal.onclick = function() {
     modal.style.display = "block";
     modalContent.innerHTML = `<video src="projects/InventoryManagement/showcase.mp4" id="modalVid" controls>`;
+}
+
+pomoModal.onclick = function() {
+    modal.style.display = "block";
+    modalContent.innerHTML = `<video src="projects/PomoPet/showcase.mp4" id="modalVid" controls>`;
 }
 
 closeSpan.onclick = function() {
